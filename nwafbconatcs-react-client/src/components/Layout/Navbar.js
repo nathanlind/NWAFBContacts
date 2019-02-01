dd207ec9f57dcdaf641eb2944f1bbfe99e6b03de
Nathan Lindsey <nathan.nwa@gmail.com>
2019-02-01 14:38:07 -0600

    Add dynamic navbar based on user login status

--- nwafbconatcs-react-client/src/components/Layout/Navbar.js
+++ nwafbconatcs-react-client/src/components/Layout/Navbar.js
@@ -1,8 +1,81 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logout } from "../../actions/securityActions";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 class Navbar extends Component {
+
+    logout(){
+        this.props.logout();
+        window.location.href = "/";
+    }
+
     render() {
+
+        const { validToken, user } = this.props.security;
+
+        const userIsAuthenticated = (
+            <div className="collapse navbar-collapse" id="mobile-nav">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/dashboard">
+                            Dashboard
+                        </Link>
+                    </li>
+                </ul>
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/dashboard">
+                            <FontAwesomeIcon icon="user" className="mr-1" />
+                            {user.fullName}
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link"
+                              to="/logout"
+                              onClick={this.logout.bind(this)}
+                        >
+                            Logout
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        );
+
+        const userIsNotAuthenticated = (
+            <div className="collapse navbar-collapse" id="mobile-nav">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/dashboard">
+                            Dashboard
+                        </Link>
+                    </li>
+                </ul>
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/register">
+                            Sign Up
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/login">
+                            Login
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        );
+
+        let navbarLinks;
+
+        if(validToken && user) {
+            navbarLinks = userIsAuthenticated;
+        } else {
+            navbarLinks = userIsNotAuthenticated;
+        }
+
         return (
             <nav className="navbar navbar-expand-sm navbar-dark navbar-color-dark mb-4">
                 <div className="container">
@@ -11,35 +84,24 @@ class Navbar extends Component {
                             NWA<span className="navbar-brand logo-font-green">FB</span>
                         </div>
                     </Link>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
-                        <span className="navbar-toggler-icon"/>
-                    </button>
-
-                    <div className="collapse navbar-collapse" id="mobile-nav">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/dashboard">
-                                    Dashboard
-                                </Link>
-                            </li>
-                        </ul>
-                        <ul className="navbar-nav ml-auto">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/register">
-                                    Sign Up
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/login">
-                                    Login
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
+                    {navbarLinks}
                 </div>
             </nav>
         );
+
+
+
     }
 }
 
-export default Navbar;
+
+Navbar.propTypes = {
+    logout: PropTypes.func.isRequired,
+    security: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    security: state.security
+});
+
+export default connect(mapStateToProps, { logout }) (Navbar);
