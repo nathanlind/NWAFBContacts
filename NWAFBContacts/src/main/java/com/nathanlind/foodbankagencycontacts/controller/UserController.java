dc6e63361d10d40000969abde3b7758417424663
Nathan Lindsey <nathan.nwa@gmail.com>
2019-01-31 18:13:04 -0600

    Adds token generation

--- NWAFBContacts/src/main/java/com/nathanlind/foodbankagencycontacts/controller/UserController.java
+++ NWAFBContacts/src/main/java/com/nathanlind/foodbankagencycontacts/controller/UserController.java
@@ -1,12 +1,19 @@
 package com.nathanlind.foodbankagencycontacts.controller;
 
 import com.nathanlind.foodbankagencycontacts.model.User;
+import com.nathanlind.foodbankagencycontacts.payload.JWTLoginSuccessResponse;
+import com.nathanlind.foodbankagencycontacts.payload.LoginRequest;
+import com.nathanlind.foodbankagencycontacts.security.JwtTokenProvider;
 import com.nathanlind.foodbankagencycontacts.service.UserService;
 import com.nathanlind.foodbankagencycontacts.service.ValidationErrorService;
 import com.nathanlind.foodbankagencycontacts.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +21,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+
+import static com.nathanlind.foodbankagencycontacts.security.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/users")
@@ -28,13 +37,42 @@ public class UserController {
     @Autowired
     private UserValidator userValidator;
 
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
+        ResponseEntity<?> errorMap = validationErrorService.MapValidationService(result);
+        if(errorMap != null) {
+            return errorMap;
+        }
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX + tokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
+    }
+
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
 
         userValidator.validate(user, result);
 
         ResponseEntity<?> errorMap = validationErrorService.MapValidationService(result);
-        if(errorMap != null)return errorMap;
+        if(errorMap != null) {
+            return errorMap;
+        }
 
         User newUser = userService.saveUser(user);
 
